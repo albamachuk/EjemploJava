3b7b18d57260a88028cee7a5a6586853999c0476
Alba Machuk <machuca.pacheco.alba@iescamas.es>
2025-09-18 19:22:45 +0200

    Add files via upload

---
+++ TipoDeDato.java
@@ -0,0 +1,26 @@
+package com;
+
+public class TipoDeDato {
+
+	public static void main(String[] args) {
+		// TIPOS DE DATOS
+		/*
+		 * esto es para poner un 
+		 * comentario mas grande
+		 * que no solo sea una linea
+		 */
+		int numero = 100; 
+		float numero_decimal = 1.1F; 
+		double numero_decimal2 = 1.1; 
+		boolean logico = false;
+		String palabra = "Tomate";
+		char letra = 'd';
+		
+		System.out.println("Numero:" + 100);
+		System.out.println("Numero decimal:" + numero_decimal);
+		System.out.println("Lógico" + logico);
+		
+
+	}
+
+}
